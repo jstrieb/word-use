d39a3a824db55e65511cd342b05503b218a80661
Jacob Strieb <jstrieb@users.noreply.github.com>
2020-12-31 15:10:16 -0500

    Initial generated file commit

---
+++ src/worduse/FXMLDocumentController.java
@@ -0,0 +1,30 @@
+/*
+ * Created by Jacob Strieb
+ * January 2021
+ */
+package worduse;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+
+/**
+ *
+ * @author jacobstrieb
+ */
+public class FXMLDocumentController implements Initializable {
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+    }
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
+    }    
+    
+}
