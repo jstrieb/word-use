3c03c8a3944d6fdfd8b3b326b35c86e9be9cbfa6
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-01-01 03:13:19 -0500

    Add browse button functionality

--- src/worduse/FXMLDocumentController.java
+++ src/worduse/FXMLDocumentController.java
@@ -4,11 +4,17 @@
  */
 package worduse;
 
+import java.io.File;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.chart.ScatterChart;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 
 /**
  *
@@ -16,7 +22,31 @@ import javafx.fxml.Initializable;
  */
 public class FXMLDocumentController implements Initializable {
     
+    @FXML
+    private AnchorPane root;
+    @FXML
+    private TextField documentPathField;
+    @FXML
+    private TextField wordField;
+    @FXML
+    private ScatterChart chart;
     
+    private File documentFile;
+    
+    @FXML
+    public void handleBrowse(ActionEvent event) {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Word Document");
+        fileChooser.getExtensionFilters().add(new ExtensionFilter("Word Documents", "*.doc", "*.docx"));
+        documentFile = fileChooser.showOpenDialog(root.getScene().getWindow());
+        
+        documentPathField.setText(documentFile.getAbsolutePath());
+    }
+    
+    @FXML
+    public void handleGraph(ActionEvent event) {
+        
+    }
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
