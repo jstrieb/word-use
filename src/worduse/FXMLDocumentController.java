15cf31fad2dba385715a63620800a499c046c627
Jacob Strieb <jstrieb@users.noreply.github.com>
2021-01-01 02:34:48 -0500

    Add FXML GUI

--- src/worduse/FXMLDocumentController.java
+++ src/worduse/FXMLDocumentController.java
@@ -9,7 +9,6 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
 
 /**
  *
@@ -17,10 +16,7 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
-    private void handleButtonAction(ActionEvent event) {
-        System.out.println("You clicked me!");
-    }
+    
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
